6c6b373e3d9d6e09373ec2d3295c513fc57837b2
DavoDev-Hub <juanpajmz13@gmail.com>
2024-11-25 02:59:57 -0600

    correccion clave expuesta

--- FrontEnd-Rip-Off/src/firebase.js
+++ FrontEnd-Rip-Off/src/firebase.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAwJNuzeq7_yBbrov7q0NAgK3DmXomubW0",
-  authDomain: "ripoff-438115.firebaseapp.com",
-  projectId: "ripoff-438115",
-  storageBucket: "ripoff-438115.appspot.com",
-  messagingSenderId: "482434096393",
-  appId: "1:482434096393:web:69840db8c733a74e4347d8",
-};
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
